45e44f5d2ff912d1c5fa8b530087ed88f984f1ae
Deividas J <deividasbox@gmail.com>
2024-05-30 13:46:55 +0300

    added AdminLTE copyright & mit license

--- resources/views/mainLayouts/app.blade.php
+++ resources/views/mainLayouts/app.blade.php
@@ -442,7 +442,7 @@ content.
 <!-- Anything you want -->
 </div>
 
-<!-- <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. -->
+<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
 </footer>
 </div>
 
